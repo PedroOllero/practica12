6cbb34ea4e31e3d273c9fbe9db9fefc4d29f7c80
Pedro Ollero <pedroollero1998@gmail.com>
2025-04-04 12:19:29 +0200

    Add hotelTourOperator class and update pricing calculation logic

--- src/main.ts
+++ src/main.ts
@@ -12,30 +12,53 @@ class hotel {
     this.tipoHabitacion = reserva.tipoHabitacion;
     this.total = 0;
     this.subtotal = 0;
-    this.pax = reserva.pax
+    this.pax = reserva.pax;
+  }
+}
+
+class hotelParticular extends hotel {
+
+  constructor(reserva: Reserva) {
+    super(reserva)
   }
 
   calculaPrecio() {
     if (this.tipoHabitacion === "standard") {
-
       this.subtotal = (100 * this.noches) + ((this.pax - 1) * 40 * this.noches);
       this.total = this.subtotal * 1.21;
     }
     if (this.tipoHabitacion === "suite") {
-        this.subtotal = 150 * this.noches;
-        this.total = this.subtotal * 1.21;
+      this.subtotal = (150 * this.noches) + ((this.pax - 1) * 40 * this.noches);
+      this.total = this.subtotal * 1.21;
     }
   }
 }
 
+class hotelTourOperator extends hotel {
+
+    constructor(reserva: Reserva) {
+      super(reserva)
+    }
+  
+    calculaPrecio() {
+        this.subtotal = (100 * this.noches) + ((this.pax - 1) * 40 * this.noches) * 0.85;
+        this.total = this.subtotal * 1.21;
+    }
+  }
+
 const reservas: Reserva = {
-  tipoHabitacion: "standard",
+  tipoHabitacion: "suite",
   desayuno: false,
   pax: 3,
-  noches: 1,
+  noches: 2,
 };
 
-const particular = new hotel(reservas);
+const particular = new hotelParticular(reservas);
 particular.calculaPrecio();
 console.log(particular.subtotal);
 console.log(particular.total);
+
+const particular2 = new hotelTourOperator(reservas);
+particular2.calculaPrecio();
+console.log(particular2.subtotal);
+console.log(particular2.total);
