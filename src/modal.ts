43b43140d824ef8896dd0711fce2fada04b74a5a
Pedro Ollero <pedroollero1998@gmail.com>
2025-04-04 10:55:33 +0200

    Refactor project structure: replace main.js with main.ts, remove dummy.spec.ts, and add modal.ts for reservation management

---
+++ src/modal.ts
@@ -0,0 +1,27 @@
+interface Reserva {
+  tipoHabitacion: "standard" | "suite";
+  desayuno: boolean;
+  pax: number;
+  noches: number;
+}
+
+const reservas = [
+  {
+    tipoHabitacion: "standard",
+    desayuno: false,
+    pax: 1,
+    noches: 3,
+  },
+  {
+    tipoHabitacion: "standard",
+    desayuno: false,
+    pax: 1,
+    noches: 4,
+  },
+  {
+    tipoHabitacion: "suite",
+    desayuno: true,
+    pax: 2,
+    noches: 1,
+  },
+];
